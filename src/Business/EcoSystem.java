f72a6b02f929ff4df42ceee9b3b67dcfc5920303
Neha Devarapalli <devarapalli.n@northeastern.edu>
2023-12-09 04:32:53 -0500

    Business configuration

---
+++ src/Business/EcoSystem.java
@@ -0,0 +1,61 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Business;
+
+import Business.Organization.Organization;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author nehadevarapalli
+ */
+public class EcoSystem extends Organization {
+
+    private static EcoSystem business;
+    private ArrayList<Network> networkList;
+
+    public static EcoSystem getInstance() {
+        if (business == null) {
+            business = new EcoSystem();
+        }
+        return business;
+    }
+
+    public Network createAndAddNetwork() {
+        Network network = new Network();
+        networkList.add(network);
+        return network;
+    }
+
+    @Override
+    public ArrayList<Role> getSupportedRole() {
+        ArrayList<Role> roleList = new ArrayList<Role>();
+        roleList.add(new SystemAdminRole());
+        return roleList;
+    }
+
+    private EcoSystem() {
+        super(null);
+        networkList = new ArrayList<Network>();
+    }
+
+    public ArrayList<Network> getNetworkList() {
+        return networkList;
+    }
+
+    public void setNetworkList(ArrayList<Network> networkList) {
+        this.networkList = networkList;
+    }
+
+    public boolean checkIfUserIsUnique(String userName) {
+        if (!this.getUserAccountDirectory().checkIfUsernameIsUnique(userName)) {
+            return false;
+        }
+        for (Network network : networkList) {
+
+        }
+        return true;
+    }
+}
