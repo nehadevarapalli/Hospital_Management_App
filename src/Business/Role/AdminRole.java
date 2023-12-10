4e88bd8eca18e7a7910a3efd8b0a1052fe6db52a
Ashish Nevan <nevchris242@gmail.com>
2023-12-09 21:15:02 -0500

    added admin

---
+++ src/Business/Role/AdminRole.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Business.Role;
+
+/**
+ *
+ * @author ashishnevan
+ */
+public class AdminRole extends Role {
+    @Override
+    public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, Organization organization, Enterprise enterprise, EcoSystem business) {
+        return new AdminWorkAreaJPanel(userProcessContainer, enterprise,account );
+    }
+}
