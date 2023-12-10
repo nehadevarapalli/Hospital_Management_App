8298ff61e2e735d262a9845af3a10b7a72bcb576
Arnav Varyani <arnav@Arnavs-MacBook-Air.local>
2023-12-09 21:04:36 -0500

    add lab organization

--- src/HealthOrganization.java
+++ src/HealthOrganization.java
@@ -1,12 +1,28 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
+package Business.Organization;
+
+import Business.Role.DoctorRole;
+import Business.Role.HealthRole;
+import Business.Role.Role;
+import java.util.ArrayList;
 
 /**
  *
  * @author arnav
  */
-public class HealthOrganization {
+public class HealthOrganization extends Organization {
+    public HealthOrganization() {
+        super(Organization.Type.Health.getValue());
+    }
     
+    @Override
+    public ArrayList<Role> getSupportedRole() {
+        ArrayList<Role> roles = new ArrayList();
+        roles.add(new HealthRole());
+        return roles;
+    }
 }
