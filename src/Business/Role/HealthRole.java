a93f607f67c81bd9b3a94d317fb5b2a8b7673001
Ashish Nevan <nevchris242@gmail.com>
2023-12-09 22:59:15 -0500

    added doctor economy health lab manufacturer person pharma sales roles

---
+++ src/Business/Role/HealthRole.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.Role;
+
+import Business.EcoSystem;
+import Business.Enterprise.Enterprise;
+import Business.Network.Network;
+import Business.Organization.Organization;
+import Business.Organization.HealthOrganization;
+import Business.UserAccount.UserAccount;
+import ui.HealthRole.HealthWorkAreaJPanel;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author aakashrajawat
+ */
+public class HealthRole extends Role{
+    
+    @Override
+    public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, Organization organization, Enterprise enterprise, EcoSystem business) {
+        return new HealthWorkAreaJPanel(userProcessContainer, account, (HealthOrganization)organization, enterprise, business);
+    }
+
+}
---
+++ src/Business/Role/LabRole.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.Role;
+
+import Business.EcoSystem;
+import Business.Enterprise.Enterprise;
+import Business.Network.Network;
+import Business.Organization.Organization;
+import Business.Organization.LabOrganization;
+import Business.UserAccount.UserAccount;
+import ui.LabRole.LabAreaJPanel;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author aakashrajawat
+ */
+public class LabRole extends Role{
+    
+    @Override
+    public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, Organization organization, Enterprise enterprise, EcoSystem business) {
+        return new LabAreaJPanel(userProcessContainer, account, (LabOrganization)organization, enterprise, business);
+    }
+
+    
+
+}
